ceb250da542fd37c47a50b0090e2d51fa063e212
cxp-13 <1695219012@qq.com>
2022-12-01 09:56:41 +0800

    1. 完成了用户模块的网络搭建

--- app/src/main/java/com/permissionx/librarymanagementsystem/logic/Repository.kt
+++ app/src/main/java/com/permissionx/librarymanagementsystem/logic/Repository.kt
@@ -1,11 +1,15 @@
 package com.permissionx.librarymanagementsystem.logic
 
+import androidx.lifecycle.liveData
 import com.permissionx.librarymanagementsystem.logic.dao.UserSPDao
 import com.permissionx.librarymanagementsystem.logic.database.AppDatabase
 import com.permissionx.librarymanagementsystem.logic.model.UserResponse
+import com.permissionx.librarymanagementsystem.logic.network.LibraryManagementSystemNetwork
 import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
 object Repository {
+
 
 
     val userDao = AppDatabase.getInstance()?.userDao()
@@ -18,8 +22,8 @@ object Repository {
         return UserSPDao.isUserSaved(name, pwd)
     }
 
-    fun saveUserDataBase(user: UserResponse.User) {
-        CoroutineScope(Dispatchers.IO).launch {
+    suspend fun saveUserDataBase(user: UserResponse.User) {
+        withContext(Dispatchers.IO) {
 
             userDao?.saveUser(user)
 
@@ -28,11 +32,45 @@ object Repository {
 
     suspend fun getUserDataBase(name: String): UserResponse.User? {
 
-
         return withContext(Dispatchers.IO) {
             userDao?.getUser(name)
         }
 
+    }
+
+    fun login(name: String, pwd: String) = fire(Dispatchers.IO) {
+        coroutineScope {
+            val response = async { LibraryManagementSystemNetwork.login(name, pwd) }.await()
+            if (response.meta.msg == "登录成功") {
+                Result.success(response.user)
+            } else {
+                Result.failure(java.lang.RuntimeException("${response.meta.msg}"))
+            }
+        }
+    }
+
+    fun registered(name: String, pwd: String) = fire(Dispatchers.IO) {
+        coroutineScope {
+            val response = async { LibraryManagementSystemNetwork.registered(name, pwd) }.await()
+            if (response.meta.msg == "注册成功") {
+                Result.success(response.token)
+            } else {
+                Result.failure(java.lang.RuntimeException("${response.meta.msg}"))
+            }
+        }
+    }
+
+
+    private fun <T> fire(context: CoroutineContext, block: suspend () -> Result<T>) {
+        liveData<Result<T>>(context) {
+            val result = try {
+                block()
+            } catch (e: Exception) {
+                Result.failure(e)
+            }
+            this.emit(result)
+
+        }
     }
 
 
--- app/src/main/java/com/permissionx/librarymanagementsystem/logic/network/LibraryManagementSystemNetwork.kt
+++ app/src/main/java/com/permissionx/librarymanagementsystem/logic/network/LibraryManagementSystemNetwork.kt
@@ -1,15 +1,34 @@
 package com.permissionx.librarymanagementsystem.logic.network
 
-object LibraryManagementSystemNetwork {
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
+object LibraryManagementSystemNetwork {
 
 
     val userService = ServiceCreator.create<UserService>()
 
+    suspend fun login(name:String, pwd:String) = userService.login(name, pwd).await()
 
+    suspend fun registered(name:String, pwd:String) = userService.registered(name, pwd).await()
 
-
-
-
-
+    private suspend fun <T> Call<T>.await(): T {
+        return suspendCoroutine {
+            enqueue(object : Callback<T> {
+                override fun onResponse(call: Call<T>, response: Response<T>) {
+                    val body = response.body()
+                    if (body == null) it.resumeWithException(RuntimeException("body is null"))
+                    else
+                        it.resume(body)
+                }
+                override fun onFailure(call: Call<T>, t: Throwable) {
+                    it.resumeWithException(t)
+                }
+            })
+        }
+    }
 }
